eabf3c82f74e4f626cdf0742c24729544f96b3cc
tiagosan040 <tiagosan040@gmail.com>
2024-02-26 09:18:08 -0300

    fix: adjust quiz card

--- src/app/meus-quizzes/quiz-card.tsx
+++ src/app/meus-quizzes/quiz-card.tsx
@@ -10,8 +10,8 @@ type QuizCardProps = {
 export const QuizCard: FC<QuizCardProps> = ({ quizDone }) => {
   return (
     <div className="max-w-[260px] h-[270px] bg-white rounded-[12px] overflow-hidden border border-gray-200 shadow-md shadow-gray-200">
-      <div className="relative">
-        <Image src={quizDone.thumbnailURL} alt={quizDone.title} width={260} height={200} />
+      <div className="relative h-[170px]">
+        <Image src={quizDone.thumbnailURL} alt={quizDone.title} fill={true} />
         <span className="absolute bg-indigo-200 text-indigo-700 p-1 rounded-md text-sm font-medium left-2 bottom-2">
           {QuizCategory[quizDone.category]}
         </span>
