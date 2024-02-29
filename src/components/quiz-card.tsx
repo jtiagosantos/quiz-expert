d89105cd8aaa94c5fb20017df85f3f4431f943b4
tiagosan040 <tiagosan040@gmail.com>
2024-02-29 17:41:47 -0300

    style: add responsive style for skeleton in quiz card

--- src/components/quiz-card.tsx
+++ src/components/quiz-card.tsx
@@ -55,7 +55,7 @@ const QuizCardTitle: FC<ComponentProps<'p'>> = ({ className, children, ...props 
 
 const QuizCardSkeleton: FC = () => {
   return (
-    <div className="max-w-[260px] w-full h-[270px] bg-gray-300 rounded-[12px] overflow-hidden border border-gray-200 shadow-md shadow-gray-200 animate-pulse">
+    <div className="max-w-[260px] w-full h-[270px] bg-gray-300 rounded-[12px] overflow-hidden border border-gray-200 shadow-md shadow-gray-200 animate-pulse max-[520px]:h-[240px]">
       <div className="w-full h-[170px] bg-gray-500 animate-pulse relative">
         <div className="w-[75px] h-[28px] rounded-md bg-gray-300 absolute left-2 bottom-2 animate-pulse" />
       </div>
