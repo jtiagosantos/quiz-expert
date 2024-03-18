62a973b5eb31e363b201b08e9133d77e32f6416e
tiagosan040 <tiagosan040@gmail.com>
2024-03-18 16:54:09 -0300

    chore: generate sitemap and robots configs

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.quizexpert.com.br/',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
